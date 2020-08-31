ca47c419ec564a5b5b53e1e58900922cc6c59508
Anton <tsymbal044@gmail.com>
2020-08-31 16:51:42 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.198bccee426e7d34fe5a4ac12f983b89.js"
 );
 
 self.addEventListener('message', (event) => {
